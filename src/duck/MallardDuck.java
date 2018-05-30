d8cf4979fc592ae787ae35920fbd456ae5ed234e
Dr. Girard <cdgira@cs.ship.edu>
2018-05-30 10:27:47 -0400

    Simple change to MallardDuck

--- src/duck/MallardDuck.java
+++ src/duck/MallardDuck.java
@@ -8,6 +8,11 @@ public class MallardDuck extends Duck
 	public MallardDuck()
 	{
 		flyBehavior = new Fly();
+		if (flyBehavior == null)
+		{
+			// Stops it from making an instance of the class
+			throw new FailedToGetBehaviorException();
+		}
 	}
 
 }
